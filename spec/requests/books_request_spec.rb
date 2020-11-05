f5dca0ccde5e33b372892654abf03c37518ab530
abe555 <riho.has3v@gmail.com>
2020-11-05 22:39:42 +0900

    Start Rspec_test_demo

--- spec/requests/books_request_spec.rb
+++ spec/requests/books_request_spec.rb
@@ -2,4 +2,18 @@ require 'rails_helper'
 
 RSpec.describe "Books", type: :request do
 
+	describe 'index画面' do
+		context 'index画面が正しく表示される' do
+			before do
+				get books_path
+			end
+			it 'リクエストは200 OKとなること' do
+				expect(response.status).to eq 200
+			end
+			it 'タイトルが正しく表示されること' do
+				expect(response.status).to include("新規投稿")
+			end
+		end
+	end
+
 end
--- spec/requests/homes_request_spec.rb
+++ spec/requests/homes_request_spec.rb
@@ -2,4 +2,14 @@ require 'rails_helper'
 
 RSpec.describe "Homes", type: :request do
 
+	describe 'Home画面' do
+		context 'Home画面が表示される' do
+			before do
+				get root_path
+			end
+			it 'リクエストは200 OKとなること' do
+				expect(response.status).to eq 200
+			end
+		end
+	end
 end
